3c67e1d0714938ad63ae1398341e14a605238b5a
admin <admin@admin-PC>
2020-03-10 18:41:35 +0800

    冒泡排序和选择排序

---
+++ src/com/mathmatic/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.mathmatic;
+/**
+ * 冒泡排序算法
+ * @author cdh
+ *
+ */
+public class BubbleSort {
+    public static void sort(long[] arr) {
+    	long temp = 0l;
+    	for(int i = 0;i<arr.length;i++) {
+    		for(int j = arr.length - 1;j>i;j--) {
+    			if(arr[j]<arr[j-1]) {
+    				temp = arr[j];
+    				arr[j]=arr[j-1];
+    				arr[j-1] = temp;
+    			}
+    		}
+    	}
+    }
+    
+    public static void main(String[] args) {
+		long[] arr = {1,6,8,3,-5};
+		BubbleSort.sort(arr);
+		System.out.print("[ ");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.print("]");
+	}
+}
---
+++ src/com/mathmatic/SelectionSort.java
@@ -0,0 +1,33 @@
+package com.mathmatic;
+/**
+ * 
+ * @author cdh
+ *选择排序
+ */
+public class SelectionSort {
+    public static void sort(long[] arr) {
+    	int k = 0;
+    	long temp;
+    	for(int i=0;i<arr.length;i++) {
+    		k = i;
+    		for(int j = i+1;j<arr.length;j++) {
+    			if(arr[j]<arr[k]) {
+    				k=j;
+    			}
+    		}
+    		temp=arr[k];
+    		arr[k]=arr[i];
+    		arr[i]=temp;
+    	}
+    }
+    
+    public static void main(String[] args) {
+    	long[] arr = {1,6,8,3,-5,89,-3};
+		SelectionSort.sort(arr);
+		System.out.print("[ ");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.print("]");
+	}
+}
